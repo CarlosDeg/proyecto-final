43ddc8014196f9d0485bc7d81a9e49ace51b9db7
l.lopezs@upam.edu.mx <https://github.com/CarlosDeg/control-scolar2.git>
2020-11-24 00:45:16 -0600

    colores del administradror inicio

--- resources/views/admin/menu.blade.php
+++ resources/views/admin/menu.blade.php
@@ -1,5 +1,5 @@
 <!-- despliege de menu -->
- <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+ <nav class="main-header navbar navbar-expand navbar-cyan navbar-light">
     <!-- Left navbar links -->
     <ul class="navbar-nav">
       <li class="nav-item">
@@ -7,15 +7,15 @@
       </li>
   </ul>
 </nav>
-<!-- Main Sidebar Container -->
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
+<!-- Main Sidebar Container -->teal
+<aside class="main-sidebar sidebar-dark-teal elevation-4 navbar-cyan">
     <!-- Brand Logo -->
     <a href="#" class="brand-link">
 
         <span class="brand-text font-weight-light  ">Administrador</span>
     </a><br><br>
 
-    <div class="sidebar "><!--MENU PARA MOSTRAR DIFERENTES MENUS DENTRO DEL ADMIN-->
+    <div class="sidebar navbar-cyan"><!--MENU PARA MOSTRAR DIFERENTES MENUS DENTRO DEL ADMIN-->
 <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
             <li class="nav-item has-treeview ">
@@ -128,7 +128,6 @@
     </li>
    </ul>
 </nav>
-        <!-- /.sidebar-menu -->
-    </div>
+
     <!-- /.sidebar -->
 </aside>
--- resources/views/admin/principal.blade.php
+++ resources/views/admin/principal.blade.php
@@ -41,11 +41,11 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 
 </head>
-<body class="hold-transition sidebar-mini" >
+<body class="hold-transition sidebar-mini navbar-light-blue" >
 
 
 
-<div class="wrapper">
+<div class="wrapper ">
 
     @extends('admin.menu')
 
